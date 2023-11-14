14f55c43087edd47430580ccd2e31a4e3d618ef6
eason-pan-ep <eason.pan@proton.me>
2023-11-14 14:12:10 -0800

    setup authentication SDK

--- firebase/firebaseSetup.js
+++ firebase/firebaseSetup.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId} from "@env";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getAuth, initializeAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const gas_pred_app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(gas_pred_app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+});
+
