84ab40d7f93695f1c7b45e26f71a047081baa291
eason-pan-ep <eason.pan@proton.me>
2023-11-14 22:40:43 -0800

    cleaned up code

--- components/SubtlePressable.js
+++ components/SubtlePressable.js
@@ -1,5 +1,12 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+// This is a pressable component that is styled to look like a text link.
+//contains the following props:
+// // title - string, the text to display on the button
+// // onPress - function, the function to call when the button is pressed
+//
+
+import { StyleSheet, Text, Pressable } from 'react-native';
 import React from 'react';
+
 import { colors } from '../styles/colors';
 import { fontSizes } from '../styles/fontSizes';
 
--- screens/TermsAndConditions.js
+++ screens/TermsAndConditions.js
@@ -1,5 +1,10 @@
+// This is a component that displays the terms and conditions of the app.
+//
+
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+
+
 
 const TermsAndConditions = () => {
   return (
