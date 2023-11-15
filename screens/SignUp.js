0fd022871c6e219844acb64e5a4e9961a8f6af8a
eason-pan-ep <eason.pan@proton.me>
2023-11-14 20:35:28 -0800

    added terms and conditions check

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,29 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import EditableField from '../components/EditableField';
 import { fontSizes } from '../styles/fontSizes';
 import { colors } from '../styles/colors';
 import { useState } from 'react';
 import CustomPressable from '../components/CustomPressable';
 import SubtlePressable from '../components/SubtlePressable';
+import Checkbox from 'expo-checkbox';
 
 const SignUp = ({navigation}) => {
     //state variables for storing user input
-    const [accountInfo, setAccountInfo] = useState({
+    const [signUpInfo, setSignUpInfo] = useState({
         email: "",
         password: "",
         confirmPassword: "",
+        isChecked: false,
     });
 
     //functions for handling user input text changes
     const handleEmailChange = (text) => {
-        setAccountInfo({ ...accountInfo, email: text });
+        setSignUpInfo({ ...signUpInfo, email: text });
     };
     const handlePasswordChange = (text) => {
-        setAccountInfo({ ...accountInfo, password: text });
+        setSignUpInfo({ ...signUpInfo, password: text });
     };
     const handleConfirmPasswordChange = (text) => {
-        setAccountInfo({ ...accountInfo, confirmPassword: text });
+        setSignUpInfo({ ...signUpInfo, confirmPassword: text });
+    };
+
+    //function for handling checkbox changes
+    const handleCheckboxChange = () => {
+        setSignUpInfo({ ...signUpInfo, isChecked: !signUpInfo.isChecked });
     };
 
     //function for handling sign up button press
@@ -31,22 +38,38 @@ const SignUp = ({navigation}) => {
         console.log("Sign Up Pressed");
     };
 
+    //function for handling sign in press
+    const handleSignInPress = () => {
+        navigation.navigate("Sign In");
+    };
+
 
     //main render
     return (
         <View style={styles.container}>
+            {/* input for email */}
             <EditableField 
                 label={"Email"} onChangeText={handleEmailChange} defaultValue={"sample@oo.com"}
                 inputType={'email-address'} isPassword={false}
             />
+            {/* input for password */}
             <EditableField 
                 label={"Password"} onChangeText={handlePasswordChange} defaultValue={"password"}
                 inputType={'default'} isPassword={true}
             />
+            {/* input for confirm password */}
             <EditableField 
                 label={"Confirm Password"} onChangeText={handleConfirmPasswordChange} defaultValue={"password"}
                 inputType={'default'} isPassword={true}
             />
+
+            {/* checkbox for terms and conditions */}
+            <View style={styles.checkBoxContainer}>
+                <Checkbox color={colors.primary} value={signUpInfo.isChecked} onValueChange={handleCheckboxChange} />
+                <Text> Agree on </Text>
+                <SubtlePressable title={"terms and conditions"} onPress={null} />
+            </View>
+            
 
             <View style={styles.buttonContainer}>
                 <CustomPressable 
@@ -55,7 +78,7 @@ const SignUp = ({navigation}) => {
                 />
                 <SubtlePressable 
                     title={"Already have an account? Sign in here."}
-                    onPress={()=>navigation.navigate('Sign In')}
+                    onPress={handleSignInPress}
                 />
             </View>
         </View>
@@ -67,9 +90,16 @@ export default SignUp;
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.background,
+        flex: 1,
     },
     buttonContainer:{
         marginTop: 20,
         alignItems: 'center',
+    },
+    checkBoxContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
     },
 });
