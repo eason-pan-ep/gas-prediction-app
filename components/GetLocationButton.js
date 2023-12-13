17639a1813f3e7874e03c783c1a79f55f21656cc
macarious <63441014+macarious@users.noreply.github.com>
2023-12-13 11:42:02 -0800

    Add map visual in NearbyGasStations screen

--- components/GetLocationButton.js
+++ components/GetLocationButton.js
@@ -1,38 +1,28 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Pressable, StyleSheet } from "react-native";
+import React from "react";
+import { Entypo } from "@expo/vector-icons";
+import * as Location from "expo-location";
 
-import * as Location from 'expo-location';
-
-import { Entypo } from '@expo/vector-icons';
+import { colors } from "../styles/colors";
 
 const GetLocationButton = ({ locationReturnHandler }) => {
-    const [status, requestPermission] = Location.useForegroundPermissions();
+  const [status, requestPermission] = Location.useForegroundPermissions();
 
-    // This function will first call another function to ask for permission to access the device's location
-    // and return the location if permission is granted
-    const getLocation = async () => {
-        try{
-            // ask for permission to access the device's location
-            if(!status.granted){
-                await requestPermission();
-                console.log("No permission, requesting permission, click button again");
-            }else{
-                // get the current location
-                const location = await Location.getCurrentPositionAsync();
-                locationReturnHandler(location);
-            }
-            
-        }catch(error){
-            console.log("Error getting user location: ", error);
-        }
-    };
-
-
-// Main render
-return (
-    <Pressable style={({pressed})=>[styles.container, {backgroundColor: pressed? '#d6d6d6':'white'}]} 
-    onPress={getLocation}>
-        <Entypo name="location" size={24} color="#258cd4" style={styles.icon} />
+  // Main render
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.container,
+        { opacity: pressed ? 0.5 : 1 },
+      ]}
+      onPress={locationReturnHandler}
+    >
+      <Entypo
+        name="location"
+        size={24}
+        color={colors.primaryDark}
+        style={styles.icon}
+      />
     </Pressable>
   );
 };
@@ -40,22 +30,22 @@ return (
 export default GetLocationButton;
 
 const styles = StyleSheet.create({
-    container: {
-        bottom: '8%',
-        right: '10%',
-        flex: 0,
-        zIndex: 1,
-        position: 'absolute',
-        shadowColor: 'black',
-        shadowOpacity: 0.2,
-        shadowOffset: {width: 0, height: 0},
-        backgroundColor: 'white',
-        borderRadius: 30,
-        elevation: 5,
-    },
-    icon: {
-        padding: 14,
-        textAlign: 'center',
-        color: '#258cd4',
-    },
+  container: {
+    bottom: "8%",
+    right: "10%",
+    flex: 0,
+    zIndex: 1,
+    position: "absolute",
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 2, height: 2 },
+    borderRadius: 30,
+    elevation: 5,
+    backgroundColor: "#ffffff",
+  },
+  icon: {
+    padding: 14,
+    textAlign: "center",
+    color: colors.primary,
+  },
 });
--- screens/NearbyGasStations.js
+++ screens/NearbyGasStations.js
@@ -2,35 +2,89 @@
 // This screen contains a map that displays the nearby gas stations.
 // This screen contains a list of the nearby gas stations.
 
-import { View, Text, Button, StyleSheet } from 'react-native';
-import React, { useState } from 'react';
-
-import GetLocationButton from '../components/GetLocationButton';
-import { getLatitude, getLongitude } from '../utility/deviceLocationUtil';
-
+import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import GetLocationButton from "../components/GetLocationButton";
+import { getLatitude, getLongitude } from "../utility/deviceLocationUtil";
+import * as Location from "expo-location";
 import { googleMapsApiKey } from "@env";
 
-
 export default function NearbyGasStations() {
-  const [locationTest, setLocationTest] = useState("");
+  const [userLocation, setUserLocation] = useState(null);
+  const [status, requestPermission] = Location.useForegroundPermissions();
 
-  const locationReturnHandler = (location) => {
-    //setLocationTest(location.coord);
-    //console.log("Location Pressed: ", location);
-    setLocationTest("Latitude: " + getLatitude(location) + ", Longitude: " + getLongitude(location));
+  // This function will run when the screen is first loaded
+  // It will obtain the user's location and store it in the userLocation state variable
+  useEffect(() => {
+    console.log("User Location: ", userLocation);
+    getLocation();
+  }, []);
+
+  // This function will ask for permission to access the device's location
+  const verifyPermission = async () => {
+    if (status.granted) {
+      return true;
+    }
+    const response = await requestPermission();
+    return response.granted;
   };
+
+  // This function will first call another function to ask for permission to access the device's location
+  // and return the location if permission is granted
+  const getLocation = async () => {
+    try {
+      const hasPermission = await verifyPermission();
+      if (!hasPermission) {
+        Alert.alert(
+          "Permission must be granted to locate nearby gas stations."
+        );
+        return;
+      }
+      // get the current location
+      const currentLocation = await Location.getCurrentPositionAsync();
+      console.log("Location Pressed: ", currentLocation);
+      setUserLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      });
+    } catch (error) {
+      console.log("Error getting user location: ", error);
+    }
+  };
+
+  const getLocationHandler = () => {
+    console.log("Get Location Pressed");
+    getLocation();
+  };
+
   return (
     <View style={styles.container}>
       <Text>NearbyGasStations</Text>
-      <GetLocationButton locationReturnHandler={locationReturnHandler} />
-      {locationTest&&<Text>Location: {locationTest}</Text>}
+      {userLocation && (
+        <Text>
+          Location: {userLocation.latitude}, {userLocation.longitude}
+        </Text>
+      )}
+      {userLocation && (
+        <Image
+          source={{
+            uri: `https://maps.googleapis.com/maps/api/staticmap?center=${userLocation.latitude},${userLocation.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${userLocation.latitude},${userLocation.longitude}&key=${googleMapsApiKey}`,
+          }}
+          style={styles.image}
+        />
+      )}
+      <GetLocationButton locationReturnHandler={getLocationHandler} />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
